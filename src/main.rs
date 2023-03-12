2f50517107ea706b87bae544b605f0ab8c322b50
Julia <bubnova.j.i@gmail.com>
2023-03-12 15:17:23 +0100

    📦️Add dice bot example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use teloxide::prelude::*;
+
+async fn run_bot() {
+    // pretty_env_logger::init();
+    // log::info!("Starting throw dice bot...");
+
+    let bot = Bot::from_env();
+
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        bot.send_dice(msg.chat.id).await?;
+        Ok(())
+    }).await;
+}
+
+
+#[tokio::main]
+async fn main() {
+    run_bot().await;
 }
